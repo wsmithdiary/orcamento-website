c6340e65a97c6878373117854d893686fa30ff05
Winston Smith <winston.19i80@gmail.com>
2026-08-26 12:03:06 -0300

    feat: adicionar identidade visual da Alicerce Empreiteira

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Formulario from "./components/Formulario";
-
+import logo from "/logo-dourado.png"
 function App() {
   return (
-    <>
+    <main
+      className="flex flex-col items-center p-28 w-dvw h-dvh bg-mist-800 text-amber-50"
+    >
+      <img src={logo} alt="teste" className="w-64" />
       <Formulario />
-    </>
+    </main>
   )
 };
 
